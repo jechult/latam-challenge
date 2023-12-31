502d3403167552e02324c60473ccec34e0a2ef9a
jmlevano <xt6774@rimac.com.pe>
2023-12-31 18:13:13 -0500

    Problem resolution from 1 to 3

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,49 @@
 from typing import List, Tuple
 
+# From utils
+from utils import init_spark
+
+# Transformation
+from pyspark.sql.functions import col, desc, explode
+
+from memory_profiler import profile
+
+file_path = "farmers-protest-tweets-2021-2-4.json"
+
+@profile
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """
+    This ``function`` reads tweets json file and process it to compute the first 10 influential users with their
+    respective count. This, in terms of memory performance.
+
+        Args:
+            file_path (str): Tweet JSON file which contains tweets from several days.
+
+        Returns:
+            q3_memory_lst (List): List of tuples containing the result from data processing.
+    """
+    spark = init_spark()
+
+    tweet_list_df = spark.read.json(file_path)
+    
+    tweet_list_df.cache().count()
+        
+    tweet_top_usr_df = tweet_list_df \
+    .select('mentionedUsers.username') \
+    .filter(col('username').isNotNull()) \
+    .select(explode('username').alias('user_name')) \
+    .groupBy('user_name') \
+    .count() \
+    .sort(desc('count')) \
+    .limit(10)
+    
+    tweet_list_df.unpersist().count()
+    
+    q3_memory_lst = tweet_top_usr_df \
+        .rdd.map(lambda x: (x[0], x[1])) \
+        .collect()
+    
+    return q3_memory_lst
+
+if __name__ == '__main__':
+    q3_memory(file_path)
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,39 @@
 from typing import List, Tuple
 
+# From utils
+from utils import init_spark
+
+# Transformation
+from pyspark.sql.functions import col, desc, explode
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """
+    This ``function`` reads tweets json file and process it to compute the first 10 influential users with their
+    respective count. This, in terms of time performance.
+
+        Args:
+            file_path (str): Tweet JSON file which contains tweets from several days.
+
+        Returns:
+            q3_time_lst (List): List of tuples containing the result from data processing.
+    """
+    spark = init_spark()
+
+    tweet_list_df = spark.read.json(file_path)
+    
+    tweet_list_df = tweet_list_df.coalesce(4)
+        
+    tweet_top_usr_df = tweet_list_df \
+    .select('mentionedUsers.username') \
+    .filter(col('username').isNotNull()) \
+    .select(explode('username').alias('user_name')) \
+    .groupBy('user_name') \
+    .count() \
+    .sort(desc('count')) \
+    .limit(10)
+    
+    q3_time_lst = tweet_top_usr_df \
+        .rdd.map(lambda x: (x[0], x[1])) \
+        .collect()
+    
+    return q3_time_lst
